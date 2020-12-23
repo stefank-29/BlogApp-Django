73d6adc620d6a8e80f6bf834c0339c5a47480045
stefan <stefankaraferovic@gmail.com>
2020-12-23 11:08:24 +0100

    reset password

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password-reset.html') , name='password-reset'),
     path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password-reset-done.html') , name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html') , name='password_reset_confirm'),
-
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html') , name='password_reset_complete'),
 ]
 
 
